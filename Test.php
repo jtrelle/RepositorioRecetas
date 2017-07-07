c8b8700c00143a15b72adf2f7971f36ad4deedeb
Jenniffer Trelles <jtrelles@espol.edu.ec>
2017-07-07 11:47:45 -0500

    Update Test.php
    cal

--- Test.php
+++ Test.php
@@ -2,8 +2,14 @@
 use Tests\TestCase;
 class Test extends PHPUnit_Framework_Test
 {
+	private $cal;
+	protected function SetUp()
+	{
+		$cal -> cal = new Cal();
+	}
 	public function testOnePlusOne() {
 		$this->assertEquals(1,2);
   	}
+	
 }
 ?>
