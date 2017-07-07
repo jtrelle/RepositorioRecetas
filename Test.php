6a23e73f64c5f3a579da34575231f689d287d355
Jenniffer Trelles <jtrelles@espol.edu.ec>
2017-07-07 12:32:26 -0500

    Update Test.php
    change

--- Test.php
+++ Test.php
@@ -5,17 +5,5 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1,2);
   	}
-	
-	public function testFailingInclude()
-	{
-		include 'not_existing_file.php';
-	}
-	
-	public function testTile()
-	{
-		$this->open("index.html");
-		$this->getTitle();
-		$this->assertEquals('Home', $title);
-	}
-}
+
 ?>
