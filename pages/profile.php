ac37bc11f23ab7a029a3c73ccd98fcd49a7514dd
BaqueLeon <mmbaque2016s1@gmail.com>
2017-08-27 23:49:45 -0500

    Add files via upload

---
+++ pages/profile.php
@@ -0,0 +1,61 @@
+<?php include('header.php'); ?>    
+    <section class="container" style="padding: 30px 0px;background-color: white;">
+        <div class="row" style="padding-top: 60px;">
+            <div class="col-xs-12 col-md-12">
+
+                <div class="col-xs-12">
+                    <div class="profile-container">
+                        <div class="profile-header">
+                            <div class="profile-picture ">
+                                <img class="img-responsive" src="https://dyd6vunh5w8vk.cloudfront.net/bonnerandpartners/uploads/2014/06/profile-none.gif">
+                            </div>
+                            <div>
+                                <h3>Juan Pérez</h3>
+                                <div>35 años</div>
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <h3>Recetas Favoritas</h3>
+                            <div class="col-md-4 col-sm-6 col-xs-12 " style="padding-top: 15px">
+                                <div class="container-receta">
+                                    <div class="bg-containerDiabeticos">
+                                        <div class="container-receta-img">
+                                            <img class="img-responsive" src="../images/recetall/caldopollo2.jpg">
+                                        </div>
+                                    </div>
+                                    <div class="recetas-contenido title-color">
+                                        <h4><strong> SOPA DE POLLO </strong></h4>
+                                        <p>Esta receta que prepararemos para que consuman todos las personas que padecen de diabetes.</p>
+                                        <div class="receta-btn"><a href="diabetes_caldo_de_pollo.php">VER MÁS</a></div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 col-sm-6 col-xs-12 " style="padding-top: 15px">
+                                <div class="container-receta">
+                                    <div class="bg-containerGastritis">
+                                        <div class="container-receta-img">
+                                            <img class="img-responsive" src="../images/recetall/sopacalabazas.jpg">
+                                        </div>
+                                    </div>
+                                    <div class="recetas-contenido title-color">
+                                        <h4><strong>SOPA DE CALABAZA Y CHAMPIÑONES</strong></h4>
+                                        <p>La calabaza es buena para combatir la gastritis, te saca el ardor del esófago y se te van las ganas de vomitar bilis</p>
+                                        <div class="receta-btn"><a href="gastritis_calabaza_y_champinones.php">VER MÁS</a></div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php include('../footer.php'); ?> 
