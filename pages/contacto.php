9af08d25a3409a4de8c47acd7ee46b3edfa320fe
BaqueLeon <mmbaque2016s1@gmail.com>
2017-08-27 23:49:07 -0500

    Add files via upload

---
+++ pages/contacto.php
@@ -0,0 +1,27 @@
+<?php include('header.php'); ?>
+
+    <!--       *******  BODY    *******     -->
+    
+    
+       <div class="container" style="padding: 30px 0px;background-color: white;">
+            <div class="row" style="padding-top: 60px;">
+                <div class="col-md-12 col-xs-12">
+                    
+                     <form >
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Mensaje:</label>
+        <textarea class="form-control" id="pwd" placeholder="Enter password" name="pwd"></textarea>
+    </div>
+    
+    <button type="submit" class="btn btn-default">ENVIAR</button>
+  </form>
+                    
+                </div>
+                </div>
+                </div>
+
+<?php include('../footer.php'); ?> 
