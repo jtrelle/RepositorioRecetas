5b14c24c002465f3526ba6743019f3627198a3e8
samichi <leonbohorquezisabel@gmail.com>
2017-09-16 17:00:08 -0500

    Update usersCrud.php

--- php/collectors/users/actions/usersCrud.php
+++ php/collectors/users/actions/usersCrud.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
+
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Choice Administration</title>
+  <title>FORM</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
