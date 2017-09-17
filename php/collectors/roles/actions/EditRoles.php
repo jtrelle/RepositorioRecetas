ae1005542e7a3ecb9fa93d78fe42d71b9022d098
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 10:35:51 -0500

    Update EditRoles.php
    Edit arreglado en el navbar

--- php/collectors/roles/actions/EditRoles.php
+++ php/collectors/roles/actions/EditRoles.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>FORM</title>
+  <title>Choice Administration</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -12,32 +13,42 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Clases PHP</a>
+      <a class="navbar-brand" href="../../../admin/adminhome.php">Administracion</a>
+
+      <a class="navbar-brand" href="../../../../index.php">Choice Home</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><p class='text-info'> Bienvenido Usuario: xxxx</p></li>
-      <li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li> 
-    </ul> 
-  </div>
-
-</nav>
 <?php
+
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
+ 
+
+  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
+  echo "<li><a href='../collectors/users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
+  echo "</ul>";
+  echo "</div>";
+  echo "</nav>";
+}
 //Obtener el valor del ID que viene del metodo GET a traves de http
 $id=$_GET["id"];
-echo "valor de id es". $id;
 
 include_once("../RolesCollector.php");
 include_once('../../../dataBase/Collector.php');
 
 $RolesCollectorObj = new RolesCollector();
 $ObjRoles = $RolesCollectorObj->showRole($id);
-print_r($ObjRoles);
-?>
 
+?>
 
 <div class="container">
   <h2>EDITAR</h2>
@@ -56,8 +67,6 @@ print_r($ObjRoles);
         <input type="text" class="form-control" id="pwd" name="role" value="<?php echo $ObjRoles->getRole(); ?>" autofocus required/>
     </div>
     </div>
-
-
     
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
