aca12e0cbceb8d229fa34b928330b1d00cf23d36
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 22:00:51 -0500

    Update portionUpdate.php
    Using alert

--- php/collectors/portions/actions/portionUpdate.php
+++ php/collectors/portions/actions/portionUpdate.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>FORM</title>
+  <title>Choice Administration</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -13,31 +12,42 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Clases PHP</a>
+      <a class="navbar-brand" href="../../../admin/adminhome.php">Administracion</a>
+
+      <a class="navbar-brand" href="../../../../index.php">Choice Home</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><p class='text-info'> Bienvenido Usuario: xxxx</p></li>
-      <li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li> 
-    </ul> 
-  </div>
-
-</nav>
-
 <?php 
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
+
+
+  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
+  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
+  echo "</ul>";
+  echo "</div>";
+  echo "</nav>";
+}
 
 $portion=$_POST["portion"];
 $id=$_POST["id"];
-echo "Edicion en proceso... </br>";
+
 include_once("../PortionCollector.php");
 $PortionCollector = new PortionCollector();
 $PortionCollector->updatePortion($id, $portion);
-echo "id: ". $id . "actualizado a: " . $portion . "</br>";
+
+echo '<script language="javascript">';
+echo 'alert("Actualizacion del id ' . $id . ' con el nombre '. $portion .' en la base de datos");document.location.href="portionVista.php"';
+echo '</script>';
 ?>
-<div><a href="portionVista.php">Volver al Inicio</a></div>
 </body>
 </html>
