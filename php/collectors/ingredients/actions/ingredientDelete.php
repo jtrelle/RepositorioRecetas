fc2aa6330d7cb62930d1921eec80827c42a36308
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 22:53:58 -0500

    Update ingredientDelete.php

--- php/collectors/ingredients/actions/ingredientDelete.php
+++ php/collectors/ingredients/actions/ingredientDelete.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>FORM</title>
+  <title>Choice Administration</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -13,16 +12,40 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="../../../admin/adminhome.php">Administracion</a>
 
+      <a class="navbar-brand" href="../../../../index.php">Choice Home</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+<?php
 
-<?php 
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+if (isset($_SESSION['MiAdmin'])){
+ 
+
+  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
+  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
+  echo "</ul>";
+  echo "</div>";
+  echo "</nav>";
+}
+
 $id=$_GET["id"];
-echo "Eliminacion en proceso... </br>";
+
 include_once("../IngredientCollector.php");
 $IngredientCollectorObj = new IngredientCollector();
 $IngredientCollectorObj->deleteIngredient($id);
-echo "Eliminado id: ". htmlspecialchars($id) . "</br>";
+echo '<script language="javascript">';
+echo 'alert("Eliminacion del id ' . $id . ' en la base de datos");document.location.href="ingredientesVista.php"';
+echo '</script>';
 ?>
-<div><a href="ingredientesVista.php">Volver al Inicio</a></div>
 </body>
 </html>
