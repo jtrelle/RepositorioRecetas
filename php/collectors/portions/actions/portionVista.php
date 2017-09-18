6b4e2e6a07608cf5f222c1f587485bed67b0b23e
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 21:44:17 -0500

    Update portionVista.php
    Portion List

--- php/collectors/portions/actions/portionVista.php
+++ php/collectors/portions/actions/portionVista.php
@@ -1,13 +1,11 @@
 <?php
-include_once("../PortionCollector.php");
-
 session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>FORM</title>
+  <title>Choice Administration</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -15,26 +13,34 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Clases PHP</a>
+      <a class="navbar-brand" href="../../../admin/adminhome.php">Administracion</a>
+
+      <a class="navbar-brand" href="../../../../index.php">Choice Home</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><p class='text-info'> Bienvenido Usuario: xxxx</p></li>
-      <li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li> 
-    </ul> 
-  </div>
-
-</nav>
-
-
+      
 <?php
 
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
 
+
+if (isset($_SESSION['MiAdmin'])){
+
+  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
+  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
+  echo "</ul>";
+  echo "</div>";
+  echo "</nav>";
+
+include_once("../PortionCollector.php");
 $id =1;
-
 $PortionCollectorObj = new PortionCollector();
   echo "<div class='container'>";
   echo "<table class='table'>";
@@ -59,20 +65,7 @@ echo "</table>";
 echo "</div>";
 echo "<div><a href='portionInsert.php' class='btn btn-info' role='button'>New Portion</a></div></div>";
 
-  echo "</ul>";
-  echo "</div>";
-  echo "</nav>";
-
-
-  ?>
-
-
-
-
+}
+?>
 </body>
 </html>
-
- 
-
-
-
