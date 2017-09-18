505eed8fba0bb72032384aa798bc76f05af0a164
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 21:59:56 -0500

    Update portionEdit.php
    Changed edit portion

--- php/collectors/portions/actions/portionEdit.php
+++ php/collectors/portions/actions/portionEdit.php
@@ -2,12 +2,10 @@
 session_start();
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>FORM</title>
+  <title>Choice Administration</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -15,32 +13,42 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Clases PHP</a>
+      <a class="navbar-brand" href="../../../admin/adminhome.php">Administracion</a>
+
+      <a class="navbar-brand" href="../../../../index.php">Choice Home</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><p class='text-info'> Bienvenido Usuario: xxxx</p></li>
-      <li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li> 
-    </ul> 
-  </div>
-
-</nav>
-
 <?php
+
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
+
+  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
+  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
+  echo "</ul>";
+  echo "</div>";
+  echo "</nav>";
+}
+
+$id =1;
 //Obtener el valor del ID que viene del metodo GET a traves de http
 $id=$_GET["id"];
-echo "valor de id es". $id;
 include_once("../PortionCollector.php");
 include_once("../../../classes/Portions.php");
 $PortionCollectorObj = new PortionCollector();
 $ObjPortion = $PortionCollectorObj->showPortion($id);
-print_r($ObjPortion);
 ?>
 <div class="container">
-  <h2>Editar el Ingrediente</h2>
+  <h2>Editar Ingrediente</h2>
   <form class="form-horizontal" action="portionUpdate.php" method="post">
     <div class="form-group">
       <label class="control-label col-sm-2" for="Usuario">Id:</label>
@@ -54,7 +62,6 @@ print_r($ObjPortion);
         <input type="text" class="form-control" id="ingrediente1" name="portion" value="<?php echo $ObjPortion->getPortion(); ?>" autofocus required/>
       </div>
     </div>    
-
 
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
