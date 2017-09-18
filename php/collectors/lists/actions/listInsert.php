1ff23832c3fb057bd91976ba747b0a9cd9262a91
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 21:25:56 -0500

    Update listInsert.php
    Changed list insert

--- php/collectors/lists/actions/listInsert.php
+++ php/collectors/lists/actions/listInsert.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>FORM</title>
+  <title>Choice Administration</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -13,25 +12,32 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Clases PHP</a>
+      <a class="navbar-brand" href="../../../admin/adminhome.php">Administracion</a>
+
+      <a class="navbar-brand" href="../../../../index.php">Choice Home</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><p class='text-info'> Bienvenido Usuario: xxxx</p></li>
-      <li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li> 
-    </ul> 
-  </div>
-
-</nav>
-
-
-
+    
 <?php
-//Obtener el valor del ID que viene del metodo GET a traves de http
+
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
+
+  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
+  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
+  echo "</ul>";
+  echo "</div>";
+  echo "</nav>";
+}
 
 $name="";
 $id="";
@@ -41,15 +47,8 @@ $ListCollectorObj = new ListCollector();
 $ObjList = new Lists($id,$name);
 ?>
 <div class="container">
-  <h2>Crear el Objeto Ingredients</h2>
+  <h2>Crear el Objeto Ingrediente</h2>
   <form class="form-horizontal" action="listCreate.php" method="post">
-
-    <div class="form-group">
-      <label class="control-label col-sm-2" for="pwd">id:</label>
-      <div class="col-sm-10">          
-        <input type="text" class="form-control" id="pwd" name="id" value="<?php echo $ObjList->setIdList($id); ?>" />
-      </div>
-      </div>
 
     <div class="form-group">
       <label class="control-label col-sm-2" for="pwd">Ingrediente:</label>
@@ -57,7 +56,6 @@ $ObjList = new Lists($id,$name);
         <input type="text" class="form-control" id="pwd" name="name" value="<?php echo $ObjList->setListName($name); ?>" />
       </div>
       </div>
-
 
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
