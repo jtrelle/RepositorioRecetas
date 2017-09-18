7151fd8623cb471258fadc2e6b08bef9e83300e8
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 21:17:48 -0500

    Update listEdit.php
    Changed EditList

--- php/collectors/lists/actions/listEdit.php
+++ php/collectors/lists/actions/listEdit.php
@@ -2,12 +2,10 @@
 session_start();
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>FORM</title>
+  <title>Choice Administration</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -15,29 +13,37 @@ session_start();
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Clases PHP</a>
+      <a class="navbar-brand" href="../../../admin/adminhome.php">Administracion</a>
+
+      <a class="navbar-brand" href="../../../../index.php">Choice Home</a>
     </div>
     <ul class="nav navbar-nav navbar-right">
-      <li><p class='text-info'> Bienvenido Usuario: xxxx</p></li>
-      <li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li> 
-    </ul> 
-  </div>
-
-</nav>
-
 <?php
+
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
+
+  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
+  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
+  echo "</ul>";
+  echo "</div>";
+  echo "</nav>";
+}
 //Obtener el valor del ID que viene del metodo GET a traves de http
 $id=$_GET["id"];
-echo "valor de id es". $id;
 include_once("../ListCollector.php");
 include_once("../../../classes/Lists.php");
 $ListCollectorObj = new ListCollector();
 $ObjList = $ListCollectorObj->showList($id);
-print_r($ObjList);
 ?>
 <div class="container">
   <h2>Editar el Ingrediente</h2>
@@ -59,7 +65,7 @@ print_r($ObjList);
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
         <button type="submit" class="btn btn-default" value="Guardar">Submit</button>
-        <a href="ingredientesVista.php">Cancelar</a>
+        <a href="listVista.php">Cancelar</a>
       </div>
     </div>
   </form>
