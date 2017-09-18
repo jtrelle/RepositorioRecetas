ec2ef03d9a2475d9ab29903989b6b19f5ad85546
samichi <leonbohorquezisabel@gmail.com>
2017-09-17 19:12:37 -0500

    Update formdemocreate.php
    Mensaje a traves de un alert

--- php/collectors/recipes_users/actions/formdemocreate.php
+++ php/collectors/recipes_users/actions/formdemocreate.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,16 +29,7 @@ if (isset($_SESSION['MiSession'])) {
       echo '</script>';
     }
 
-
 if (isset($_SESSION['MiAdmin'])){
-
-
-  
-
-
-
-  #echo "<p> Hola Usuario: " . $_SESSION['MiSesion']. "    <a href='salir.php' class='btn btn-info' role='button'>Salir</a>";
- 
 
   echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
   echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
@@ -45,20 +37,20 @@ if (isset($_SESSION['MiAdmin'])){
   echo "</div>";
   echo "</nav>";
 }
-
+      
 $recipes_id=$_POST["recipes_id"];
 $users_id=$_POST["users_id"];
 
-echo "Insercion de datos en proceso... </br>";
-
 include_once("../RecipesUsersCollector.php");
-
 
 $RecipesUsersCollectorObj = new RecipesUsersCollector();
 $RecipesUsersCollectorObj->createRecipesUsers($recipes_id,$users_id);
 
+echo '<script language="javascript">';
+echo 'alert("Creación de la informacion en la base de datos");document.location.href="ListRecipesUsers.php"';
+echo '</script>';
 
 ?>
-<div><a href="ListRecipesUsers.php" class="btn btn-info" role="button">Volver al Inicio</a></div>
+
 </body>
 </html>
