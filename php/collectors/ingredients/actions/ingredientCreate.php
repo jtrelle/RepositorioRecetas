decfebe20a56d87eb50b34b6d1c605ab118992fa
BaqueLeon <mmbaque2016s1@gmail.com>
2017-09-19 15:00:11 -0500

    Update ingredientCreate.php

--- php/collectors/ingredients/actions/ingredientCreate.php
+++ php/collectors/ingredients/actions/ingredientCreate.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){ 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,14 +30,7 @@ session_start();
     <ul class="nav navbar-nav navbar-right">
 <?php
 
-if (isset($_SESSION['MiSession'])) {
-      echo '<script language="javascript">';
-      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-      echo '</script>';
-    }
 
-
-if (isset($_SESSION['MiAdmin'])){ 
 
   echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
   echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
@@ -51,7 +52,8 @@ echo '<script language="javascript">';
 echo 'alert("Creación de '. $ingredient .' en la base de datos");document.location.href="ingredientesVista.php"';
 echo '</script>';
 
-}?>
+
 </div>
 </body>
 </html>
+<?php } ?>
