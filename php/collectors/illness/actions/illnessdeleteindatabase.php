de97d9187758b74446010c369d1700995ddec0e0
samichi <leonbohorquezisabel@gmail.com>
2017-09-19 16:12:22 -0500

    Update illnessdeleteindatabase.php

--- php/collectors/illness/actions/illnessdeleteindatabase.php
+++ php/collectors/illness/actions/illnessdeleteindatabase.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,14 +30,7 @@ session_start();
     <ul class="nav navbar-nav navbar-right">
 <?php
 
-if (isset($_SESSION['MiSession'])) {
-      echo '<script language="javascript">';
-      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-      echo '</script>';
-    }
 
-
-if (isset($_SESSION['MiAdmin'])){
  
 
   echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
