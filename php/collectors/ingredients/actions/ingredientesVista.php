eb5ae0bdc3762fccc0a6975247ce0183b7835ca6
BaqueLeon <mmbaque2016s1@gmail.com>
2017-09-19 15:08:02 -0500

    Update ingredientesVista.php

--- php/collectors/ingredients/actions/ingredientesVista.php
+++ php/collectors/ingredients/actions/ingredientesVista.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
 ?>
 
 <!DOCTYPE html>
@@ -26,14 +35,7 @@ session_start();
 
 <?php
 
-if (isset($_SESSION['MiSession'])) {
-      echo '<script language="javascript">';
-      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-      echo '</script>';
-    }
 
-
-if (isset($_SESSION['MiAdmin'])){
 
   echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
   echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
@@ -74,7 +76,9 @@ echo "<div><a href='ingredientInsert.php' class='btn btn-info' role='button'>New
   echo "</ul>";
   echo "</div>";
   echo "</nav>";
-}
+
 ?>
 </body>
 </html>
+  
+<?php } ?>
