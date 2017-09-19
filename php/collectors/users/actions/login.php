777b4052ddae37d0f17fa373178df48c1f4e25f1
Josselyn Villacreses <jorevill@espol.edu.ec>
2017-09-19 14:45:38 -0500

    Update login.php

--- php/collectors/users/actions/login.php
+++ php/collectors/users/actions/login.php
@@ -1,6 +1,16 @@
 <?php
 	
 
+if (isset($_SESSION['MiSession'])) {
+			echo '<script language="javascript">';
+			echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+			echo '</script>';
+		}
+
+
+if (isset($_SESSION['MiAdmin'])){
+
+
  session_start();
 	$username = $_POST['user'];
 	$password = $_POST['password'];
@@ -36,6 +46,6 @@
       header("Location:../../../../index.php");
 	}
   	
- 
+}
 
 ?>
