066d769feef27851bb5f8c5e3bd47625f63693ad
samichi <leonbohorquezisabel@gmail.com>
2017-09-19 15:34:52 -0500

    Update portionDelete.php

--- php/collectors/portions/actions/portionDelete.php
+++ php/collectors/portions/actions/portionDelete.php
@@ -22,14 +22,7 @@ session_start();
     <ul class="nav navbar-nav navbar-right">
 <?php
 
-if (isset($_SESSION['MiSession'])) {
-      echo '<script language="javascript">';
-      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-      echo '</script>';
-    }
 
-if (isset($_SESSION['MiAdmin'])){
- 
 
   echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
   echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
@@ -48,7 +41,7 @@ $PortionCollectorObj->deletePortion($id);
 echo '<script language="javascript">';
 echo 'alert("Eliminacion del id ' . $id . ' en la base de datos");document.location.href="portionVista.php"';
 echo '</script>';
-}?>
+?>
 
 </body>
 </html>
