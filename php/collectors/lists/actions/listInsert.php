bae3f3760b6618f2611261fcba8660faa95d5cf4
Jenniffer Trelles <jtrelles@espol.edu.ec>
2017-09-19 15:35:06 -0500

    Update listInsert.php

--- php/collectors/lists/actions/listInsert.php
+++ php/collectors/lists/actions/listInsert.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+
+
+if (isset($_SESSION['MiAdmin'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,14 +31,7 @@ session_start();
     
 <?php
 
-if (isset($_SESSION['MiSession'])) {
-      echo '<script language="javascript">';
-      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-      echo '</script>';
-    }
 
-
-if (isset($_SESSION['MiAdmin'])){
 
   echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
   echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
@@ -65,6 +66,7 @@ $ObjList = new Lists($id,$name);
     </div>
   </form>
 </div>
-        }?>
+   ?>
 </body>
 </html>
+  <?php } ?>
