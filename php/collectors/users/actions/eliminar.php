14ab88ad2137fb92e4e6c2b3d8411943ac64e8f2
Josselyn Villacreses <jorevill@espol.edu.ec>
2017-09-19 14:44:58 -0500

    Update eliminar.php

--- php/collectors/users/actions/eliminar.php
+++ php/collectors/users/actions/eliminar.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+if (isset($_SESSION['MiSession'])) {
+			echo '<script language="javascript">';
+			echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+			echo '</script>';
+		}
+
+
+if (isset($_SESSION['MiAdmin'])){
+
 
 $id=$_GET["id"];
 include_once('../UserCollector.php');
@@ -9,6 +18,6 @@ $UserCollectorObj->deleteUser($id);
 echo '<script language="javascript">';
 			echo 'alert("El usuario se ha eliminado.");document.location.href="usersCrud.php"';
 			echo '</script>';
-
+}
 ?>
 
