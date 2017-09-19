9cf2367150294a3b0aaba78e9d723eb97de5ede3
Josselyn Villacreses <jorevill@espol.edu.ec>
2017-09-19 14:36:45 -0500

    Update cambioRol.php

--- php/collectors/users/actions/cambioRol.php
+++ php/collectors/users/actions/cambioRol.php
@@ -1,5 +1,17 @@
 <?php
 
+if (isset($_SESSION['MiSession'])) {
+			echo '<script language="javascript">';
+			echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+			echo '</script>';
+		}
+
+
+if (isset($_SESSION['MiAdmin'])){
+
+
+
+
 include_once("../UserCollector.php");
 $id=$_GET["id"];
 
@@ -12,5 +24,5 @@ $UserCollectorObj->cambiarRol($id, $_POST['comboRoles']);
   echo '</script>';
 
 
-
+}
 ?>
