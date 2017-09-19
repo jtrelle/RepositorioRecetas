b6a4767767794dc2da905fa48546f7f59fcc6282
Jenniffer Trelles <jtrelles@espol.edu.ec>
2017-09-19 15:06:01 -0500

    Update formdemocreate.php

--- php/collectors/recipes_users/actions/formdemocreate.php
+++ php/collectors/recipes_users/actions/formdemocreate.php
@@ -1,18 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION['MiSession'])) {
-      echo '<script language="javascript">';
-      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-      echo '</script>';
-    }
 
-if (isset($_SESSION['MiAdmin'])){
-
-  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
-  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
-  echo "</ul>";
-  echo "</div>";
-  echo "</nav>";
 
 ?>
 
@@ -56,5 +44,4 @@ echo '</script>';
 
 </body>
 </html>
-  
-  <?php } ?>
+
