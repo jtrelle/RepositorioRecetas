ef9e1cde671895f75955adc3e083cff74d4fcf17
Josselyn Villacreses <jorevill@espol.edu.ec>
2017-09-19 14:40:17 -0500

    Update actualizarCuenta.php

--- php/collectors/users/actions/actualizarCuenta.php
+++ php/collectors/users/actions/actualizarCuenta.php
@@ -4,13 +4,6 @@
 
 
 
-if (isset($_SESSION['MiSession'])) {
-			echo '<script language="javascript">';
-			echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-			echo '</script>';
-		}
-if (isset($_SESSION['MiAdmin'])){
-
 
 	 $username=$_POST['username'];
 	 $pass=$_POST['pass'];
@@ -55,5 +48,5 @@ if (isset($_SESSION['MiAdmin'])){
 			echo '</script>';
 	}
 
-}			
+			
 ?>
