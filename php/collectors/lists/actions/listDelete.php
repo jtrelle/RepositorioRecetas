7f80a016167a15442e90e391d468aa73088a9a00
BaqueLeon <mmbaque2016s1@gmail.com>
2017-09-19 16:41:02 -0500

    Update listDelete.php

--- php/collectors/lists/actions/listDelete.php
+++ php/collectors/lists/actions/listDelete.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+if (isset($_SESSION['MiSession'])) {
+      echo '<script language="javascript">';
+      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
+      echo '</script>';
+    }
+if (isset($_SESSION['MiAdmin'])){
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,21 +30,6 @@ session_start();
     <ul class="nav navbar-nav navbar-right">
 <?php
 
-if (isset($_SESSION['MiSession'])) {
-      echo '<script language="javascript">';
-      echo 'alert("No tiene autorización para esta página.");document.location.href="../../../../index.php"';
-      echo '</script>';
-    }
-
-
-if (isset($_SESSION['MiAdmin'])){
- 
-
-  echo "<li><p class='navbar-brand'> Bienvenido Usuario: " . $_SESSION['MiAdmin'] . "</p></li>";
-  echo "<li><a href='../../users/actions/salir.php'><span class='glyphicon glyphicon-log-out'></span> Salir </a></li>";
-  echo "</ul>";
-  echo "</div>";
-  echo "</nav>";
 
 
 $id=$_GET["id"];
@@ -56,3 +49,7 @@ echo '</script>';*/
 
 </body>
 </html>
+  <?php } else 
+{
+  echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../../../../index.php'>";     
+}?>
