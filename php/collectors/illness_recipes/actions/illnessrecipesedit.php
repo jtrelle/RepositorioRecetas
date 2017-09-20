d2d890224c5a0c61507470043db9a24a78b3b77d
BaqueLeon <mmbaque2016s1@gmail.com>
2017-09-19 23:56:28 -0500

    Update illnessrecipesedit.php

--- php/collectors/illness_recipes/actions/illnessrecipesedit.php
+++ php/collectors/illness_recipes/actions/illnessrecipesedit.php
@@ -148,4 +148,7 @@ $recipesList=$recipes->showsRecipes();
 </div>
 </body>
 </html>
-<?php } ?>
+<?php } else 
+{
+  echo "<meta HTTP-EQUIV='REFRESH' CONTENT='1;URL=../../../../index.php'>";     
+} ?> 
